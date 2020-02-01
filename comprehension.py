7c9e8bd39ff0473682ce15dee63af83e694349c3
Ferdeen Mughal <ferdeen@gmail.com>
2020-02-01 21:28:47 +0000

    use pp

--- comprehension.py
+++ comprehension.py
@@ -42,7 +42,7 @@ def fileSizeExample():
 
     import os
     import glob
-    from pprint import pprint as pp
+    from pprint import pprint as pp 
 
     file_sizes = {os.path.realpath(p): os.stat(p).st_size
                     for p in glob.glob('*.py')}
