ba9dac0f8a5f5a61f846ba699ac85f229583143c
Ferdeen Mughal <ferdeen@gmail.com>
2020-02-02 16:21:09 +0000

    add validation in the initialiser

--- classes/airtravel.py
+++ classes/airtravel.py
@@ -2,8 +2,23 @@
 
 class Flight:
     
+    def __init__(self, number):
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code in '{}'".format(number))
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("No airline code in '{}'".format(number))
+
+        self._number = number
+
     def number(self):
-        return "SN060"
+        return self._number
+
+    def airline(self):
+        return self._number[:2]
 
 
 
